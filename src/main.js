c995df3fd3fb87734246fc3db3f164e86c292be1
Larissa Silva <larissalarissasilva62@gmail.com>
2023-01-18 18:22:39 -0300

    Criando a listagem de produtos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const addChild = async () => {
+  const fetchList = await fetchProductsList('computador');
+  return fetchList.forEach((element) => {
+    const secProduc = document.querySelector('.products');
+    secProduc.appendChild(createProductElement(element));
+  });
+};
+
+addChild();
