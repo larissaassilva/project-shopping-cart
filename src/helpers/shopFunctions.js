da9b2749c30ca3e99ebc2eb0dded1a05c5c79e4b
Larissa Silva <larissalarissasilva62@gmail.com>
2023-01-18 22:32:34 -0300

    Agora o produto pode ser adicionado ao carrinho de compras

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -121,6 +122,16 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'product__add',
     'Adicionar ao carrinho!',
   );
+
+  cartButton.addEventListener('click', async (event) => {
+    const i = event.target.parentNode.firstElementChild.innerText;
+    saveCartID(i);
+    const detProduc = await fetchProduct(i);
+    const product = createCartProductElement(detProduc);
+    const ol = document.querySelector('.cart__products');
+    ol.appendChild(product);
+  });
+
   section.appendChild(cartButton);
 
   return section;
